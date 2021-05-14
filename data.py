35d2be87177a31eac568a85d3f50f3208dfe50e1
gullerg <gustavmgrimberg@gmail.com>
2021-05-14 19:20:36 +0100

    .

---
+++ data.py
@@ -0,0 +1,39 @@
+import torch
+import glob
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'-"
+n_letters = len(all_letters)
+
+def findFiles(path): return glob.glob(path)
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+    
+category_lines = {}
+all_categories = []
+for filename in findFiles('data\\names\\*.txt'):
+    category = filename.split('\\')[-1].split('.')[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
